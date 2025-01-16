406892e022cf05fc6a6e7290827c1f2b84ac8532
Alexandru Grecu <alexandru.grecu27@gmail.com>
2025-01-16 17:49:00 +0200

    nothing

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,26 +5,29 @@ import { ChartConfig, ChartContainer, ChartLegend, ChartLegendContent } from "@/
 
 const chartData = [
   {
-    "label": "Label1",
-    "Category1": 12
+    "label": "January",
+    "Category A": 12,
+    "Category B": 8
   },
   {
-    "label": "Label2",
-    "Category1": 5
+    "label": "February",
+    "Category A": 15,
+    "Category B": 6
   },
   {
-    "label": "Label3",
-    "Category1": 9
-  },
-  {
-    "label": "Label4",
-    "Category1": 6
+    "label": "March",
+    "Category A": 9,
+    "Category B": 14
   }
 ]
 
 const chartConfig = {
-  "Category1": {
-    "label": "Category1",
+  "Category A": {
+    "label": "Category A",
+    "color": "hsl(var(--chart-1))"
+  },
+  "Category B": {
+    "label": "Category B",
     "color": "hsl(var(--chart-3))"
   }
 } satisfies ChartConfig
@@ -33,7 +36,8 @@ export default function Component() {
   return (
     <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
       <BarChart accessibilityLayer data={chartData}>
-        <Bar dataKey="Category1" fill="var(--color-Category1)" radius={4} />
+        <Bar dataKey="Category A" fill="var(--color-Category A)" radius={4} />
+        <Bar dataKey="Category B" fill="var(--color-Category B)" radius={4} />
         <XAxis dataKey="label" />
         <YAxis stroke="#333" />
         <ChartLegend content={<ChartLegendContent />} />
---
+++ supabase/seed.sql
@@ -0,0 +1,203 @@
+SET session_replication_role = replica;
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.8
+-- Dumped by pg_dump version 15.6
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: audit_log_entries; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: flow_state; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: identities; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: instances; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: sessions; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: mfa_amr_claims; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: mfa_factors; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: mfa_challenges; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: one_time_tokens; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: refresh_tokens; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: sso_providers; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: saml_providers; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: saml_relay_states; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: sso_domains; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+
+
+--
+-- Data for Name: key; Type: TABLE DATA; Schema: pgsodium; Owner: supabase_admin
+--
+
+
+
+--
+-- Data for Name: customers; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO "public"."products" ("id", "active", "name", "description", "image", "metadata") VALUES
+	('prod_RajZzRnkic317e', true, 'Hobby', 'Hobby product description', NULL, '{"index": "0"}'),
+	('prod_RajZs7ZdBdfztF', true, 'Freelancer', 'Freelancer product description', NULL, '{"index": "1"}');
+
+
+--
+-- Data for Name: prices; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO "public"."prices" ("id", "product_id", "active", "description", "unit_amount", "currency", "type", "interval", "interval_count", "trial_period_days", "metadata") VALUES
+	('price_1QhY6hDCKLBnj8LTL4nStS9E', 'prod_RajZzRnkic317e', true, NULL, 1000, 'usd', 'recurring', 'month', 1, 0, NULL),
+	('price_1QhY6iDCKLBnj8LT18XKtnuK', 'prod_RajZzRnkic317e', true, NULL, 10000, 'usd', 'recurring', 'year', 1, 0, NULL),
+	('price_1QhY6iDCKLBnj8LTUkzkE9wM', 'prod_RajZs7ZdBdfztF', true, NULL, 2000, 'usd', 'recurring', 'month', 1, 0, NULL),
+	('price_1QhY6jDCKLBnj8LTMJ5qK4oL', 'prod_RajZs7ZdBdfztF', true, NULL, 20000, 'usd', 'recurring', 'year', 1, 0, NULL);
+
+
+--
+-- Data for Name: subscriptions; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: buckets; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
+--
+
+
+
+--
+-- Data for Name: objects; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
+--
+
+
+
+--
+-- Data for Name: s3_multipart_uploads; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
+--
+
+
+
+--
+-- Data for Name: s3_multipart_uploads_parts; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
+--
+
+
+
+--
+-- Data for Name: secrets; Type: TABLE DATA; Schema: vault; Owner: supabase_admin
+--
+
+
+
+--
+-- Name: refresh_tokens_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: supabase_auth_admin
+--
+
+SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 1, false);
+
+
+--
+-- Name: key_key_id_seq; Type: SEQUENCE SET; Schema: pgsodium; Owner: supabase_admin
+--
+
+SELECT pg_catalog.setval('"pgsodium"."key_key_id_seq"', 1, false);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+RESET ALL;
